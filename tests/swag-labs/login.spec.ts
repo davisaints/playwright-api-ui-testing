fa05c4aa0bfa7bee523d7952c7ea08841b20238c
Davi Santos <davi.santos4357@gmail.com>
2024-11-07 22:01:59 -0300

    test: Add successful login test

---
+++ tests/swag-labs/login.spec.ts
@@ -0,0 +1,27 @@
+import { expect, mergeTests } from "@playwright/test";
+
+import { swagLabsPagesTest } from "./fixtures/swagLabsPagesTest";
+
+export const test = mergeTests(swagLabsPagesTest);
+
+test.beforeEach(async ({ loginPage }) => {
+  await test.step("Given the user navigates to the login page", async () => {
+    await loginPage.goToLoginPage();
+  });
+});
+
+test.describe("Login functionality", () => {
+  test("User is redirected to the Products page when logged in with valid credentials", async ({
+    loginPage,
+    page,
+  }) => {
+    await test.step("When the user logs in with valid credentials", async () => {
+      await loginPage.loginAs("Standart user");
+    });
+
+    await test.step("Then the user is redirected to the products page and sees the Products header", async () => {
+      const productPageHeader = page.getByText("Products");
+      await expect(productPageHeader).toBeVisible();
+    });
+  });
+});

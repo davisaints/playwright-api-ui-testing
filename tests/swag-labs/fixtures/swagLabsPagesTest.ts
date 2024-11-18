8e1a985241ea46e57480ee921f9c181d1f8e47dc
Davi Santos <davi.santos4357@gmail.com>
2024-11-18 20:56:07 -0300

    feat: Add POM for checkout page

--- tests/swag-labs/fixtures/swagLabsPagesTest.ts
+++ tests/swag-labs/fixtures/swagLabsPagesTest.ts
@@ -1,12 +1,17 @@
 import { test } from "@playwright/test";
 
+import { CheckoutPage } from "../pages/CheckoutPage";
 import { InventoryPage } from "../pages/InventoryPage";
 import { LoginPage } from "../pages/LoginPage";
 
 const swagLabsPagesTest = test.extend<{
+  checkoutPage: CheckoutPage;
   inventoryPage: InventoryPage;
   loginPage: LoginPage;
 }>({
+  checkoutPage: async ({ page }, use) => {
+    await use(new CheckoutPage(page));
+  },
   inventoryPage: async ({ page }, use) => {
     await use(new InventoryPage(page));
   },
--- tests/swag-labs/pages/CheckoutPage.ts
+++ tests/swag-labs/pages/CheckoutPage.ts
@@ -1,9 +1,59 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class CheckoutPage {
   private readonly page: Page;
 
+  public readonly checkoutButton: Locator;
+  public readonly continueButton: Locator;
+  private readonly shopCartLink: Locator;
+  public readonly finishButton: Locator;
+
   constructor(page: Page) {
     this.page = page;
+    this.checkoutButton = page.getByRole("button", {
+      name: "Checkout",
+    });
+    this.continueButton = page.getByRole("button", {
+      name: "Continue",
+    });
+    this.shopCartLink = page.locator(".shopping_cart_link");
+    this.finishButton = page.getByRole("button", {
+      name: "Finish",
+    });
+  }
+
+  private productCardLocator(productName: string) {
+    return this.page.locator(".inventory_item", {
+      hasText: productName,
+    });
+  }
+
+  async addProductToCart(productName: string) {
+    const addToCartButton =
+      this.productCardLocator(productName).getByText("Add to cart");
+
+    await addToCartButton.click();
+  }
+
+  async fillCheckoutInfo(firstName: string, lastName: string, code: string) {
+    await this.page.getByPlaceholder("First Name").fill(firstName);
+
+    await this.page.getByPlaceholder("Last Name").fill(lastName);
+
+    await this.page.getByPlaceholder("Zip/Postal Code").fill(code);
+  }
+
+  async openCart() {
+    await this.shopCartLink.click();
+
+    await this.page.getByText("Your Cart").waitFor();
+  }
+
+  async removeProductFromCart(productName) {
+    const removeButton = this.page
+      .locator(".cart_item", { hasText: productName })
+      .getByText("Remove");
+
+    await removeButton.click();
   }
 }

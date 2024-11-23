8d94ad3782bf4df98a4617d8c32adf8e584f88d1
Davi Santos <davi.santos4357@gmail.com>
2024-11-23 13:53:26 -0300

    style: SF

--- tests/swag-labs/config.ts
+++ tests/swag-labs/config.ts
@@ -1,9 +1,9 @@
-import {devices} from '@playwright/test';
+import { devices } from "@playwright/test";
 
 export const config = {
-	name: 'swag-labs',
-	testDir: 'tests/swag-labs',
-	use: {
-		...devices['Desktop Chrome'],
-	},
+  name: "swag-labs",
+  testDir: "tests/swag-labs",
+  use: {
+    ...devices["Desktop Chrome"],
+  },
 };

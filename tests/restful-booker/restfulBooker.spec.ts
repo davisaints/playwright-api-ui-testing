c0548f7036473239f6a7393fec70e29700c343a5
Davi Santos <davi.santos4357@gmail.com>
2024-11-27 19:44:02 -0300

    refactor: Add test to verify booking removal

--- tests/restful-booker/restfulBooker.spec.ts
+++ tests/restful-booker/restfulBooker.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from "@playwright/test";
 import { ApiHelpers } from "../restful-booker/helpers/ApiHelpers";
 import { booking } from "./test-data/booking";
 
+const HTTP_STATUS_CREATED = 201;
+
 const apiHelpers = new ApiHelpers();
 
 test.beforeAll(async () => {
@@ -15,6 +17,16 @@ test("Can create booking", async () => {
   const createdBookingJson = await createBookingResponse.json();
   expect(createdBookingJson.bookingid).toBeDefined();
   expect(createdBookingJson.booking).toMatchObject(booking);
+});
+
+test("Can delete booking", async () => {
+  const createBookingResponse = await apiHelpers.createBooking(booking);
+  const createdBookingJson = await createBookingResponse.json();
+  const bookingId = createdBookingJson.bookingid;
+
+  console.log(bookingId)
+  const deleteBookingResponse = await apiHelpers.deleteBooking(bookingId);
+  expect(deleteBookingResponse.status()).toBe(HTTP_STATUS_CREATED);
 });
 
 test("Can get booking", async () => {
